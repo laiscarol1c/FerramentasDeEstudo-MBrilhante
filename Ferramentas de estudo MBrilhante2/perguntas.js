4b6e26e187d79d22f3c2be3b8b497ccbfda7bcef
laiscarol1c <00001132601095sp@al.educacao.sp.gov.br>
2024-11-28 10:13:05 -0300

    Add files via upload

---
+++ Ferramentas de estudo MBrilhante2/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é GitHub?',
+    'O Github é um repositorio de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a diferença de clima e tempo?',
+    'Circulo é o preenchimento da cio tempo se refere às condições meteorológicas do momento em que a observação é realizada.rcunferência e circunferência e o contorno'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz pessoas em Inglês?',
+    'pessoas em ingles é people (pipou)'
+)
